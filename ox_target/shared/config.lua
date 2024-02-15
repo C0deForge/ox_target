ceae339af5eeed23311cc5b2baf0cab8fe49c0d8
CodeForge <125872426+C0deForge@users.noreply.github.com>
2024-02-15 21:28:27 +0100

    Add files via upload

---
+++ ox_target/shared/config.lua
@@ -0,0 +1,12 @@
+Config = { }
+
+Config.Style = {
+    eyeBackground = 'linear-gradient(90deg, #302B27 19.79%, rgba(0, 0, 0, 0.14) 100%)',
+    eyeBorder = '#5E5E5E',
+    eyePrimary = '#FF0066',
+    primary = 'linear-gradient(270deg, rgba(255, 0, 102, 0) 0%, rgba(255, 0, 102, 0.9) 98.22%)',
+    secondary = 'linear-gradient(270deg, rgba(79, 79, 79, 0) 0%, rgba(105, 105, 105, 0.384) 98.22%)',
+    background = 'rgba(27, 27, 27, 0.7)',
+    borderSecondary = 'rgba(123, 144, 183, 0.3)',
+    borderPrimary = '#FF0066'
+}
